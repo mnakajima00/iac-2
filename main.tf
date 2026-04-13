fd5f29c5d1a472a5fa7f9f7385710d8aac18f0a4
mnakajima00 <maikunakajima.work@gmail.com>
2026-04-12 23:42:26 -0400

    chore: add labels to invoice archive bucket

--- main.tf
+++ main.tf
@@ -595,6 +595,11 @@ resource "google_storage_bucket" "invoice_archive" {
 
   uniform_bucket_level_access = true
 
+  labels = {
+    environment = "production"
+    service     = "billing"
+  }
+
   versioning {
     enabled = true
   }
